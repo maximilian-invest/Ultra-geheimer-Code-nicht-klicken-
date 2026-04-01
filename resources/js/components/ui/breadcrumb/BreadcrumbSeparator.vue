c141a587b136aad9ac1d5404081880bff37eece0
root <root@srv1495003.hstgr.cloud>
2026-04-01 20:23:22 +0200

    feat: add shadcn switch, breadcrumb, toggle-group components

---
+++ resources/js/components/ui/breadcrumb/BreadcrumbSeparator.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ChevronRight } from "lucide-vue-next";
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  class: {
+    type: [Boolean, null, String, Object, Array],
+    required: false,
+    skipCheck: true,
+  },
+});
+</script>
+
+<template>
+  <li
+    role="presentation"
+    aria-hidden="true"
+    :class="cn('[&>svg]:size-3.5', props.class)"
+  >
+    <slot>
+      <ChevronRight />
+    </slot>
+  </li>
+</template>

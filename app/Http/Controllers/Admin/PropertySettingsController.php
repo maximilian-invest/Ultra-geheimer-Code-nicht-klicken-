458138f99b4dd443094eeb7d34c538cad1e7b7d2
root <root@srv1495003.hstgr.cloud>
2026-04-02 23:37:53 +0200

    save_property_unit: save portal_exports (json_encode) and immoji_id from request

--- app/Http/Controllers/Admin/PropertySettingsController.php
+++ app/Http/Controllers/Admin/PropertySettingsController.php
@@ -574,6 +574,8 @@ class PropertySettingsController extends Controller
             'garden_m2'          => isset($data['garden_m2']) ? floatval($data['garden_m2']) : null,
             'parking'            => trim($data['parking'] ?? '') ?: null,
             'notes'              => trim($data['notes'] ?? '') ?: null,
+            'portal_exports'     => isset($data['portal_exports']) ? json_encode($data['portal_exports']) : null,
+            'immoji_id'          => trim($data['immoji_id'] ?? '') ?: null,
             'assigned_parking'   => isset($data['assigned_parking']) ? $data['assigned_parking'] : null,
             'images'             => isset($data['images']) ? json_encode($data['images']) : null,
             'buyer_name'         => trim($data['buyer_name'] ?? '') ?: null,
